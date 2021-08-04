4ed072beb7495fd2eb7599ba7cf1c536a16195b9
readcreate <78000656+readcreate@users.noreply.github.com>
2021-08-04 01:27:25 -0400

    Add files via upload

--- R/crossValidate.R
+++ R/crossValidate.R
@@ -1,6 +1,6 @@
 #' Cross Validate Predictive Models
 #'
-#' Perform "manual" cross validation. This function automatically creates folds (groups) in your data to cross-validate predictive models. It executes provided code, repeatedly on each training/testing data split, using each fold as the testing data one time. Returns a data.frame with original data (in original order) and the following additional variables: predicted values of the dependent variable, new ID number (according to the reordered data), original order number, and fold number.
+#' Perform "manual" cross validation. This function automatically creates folds (groups) in your data to cross-validate predictive models. It executes provided code, repeatedly on each training/testing data split, using each fold as the testing data one time. Returns a data.frame with original data (in original order) and the predicted values of the dependent variable.
 #'
 #' @param df data.frame containing all observations, independent variables, and dependent variable
 #' @param codeString string containing code to run your ML model, which this function will run for you on each fold. Separate lines with a semicolon. Anywhere you write `traindata`, this function will replace it with the training portion of your data for each fold. Anywhere you write `testdata`, this function will replace it with the current testing fold in the cross validation. Variables you add to `testdata` in your code will be included in the returned data.frame.
@@ -19,15 +19,21 @@
 #' Metrics::rmse(mtcars.cv5.rf$rf.pred.mpg, mtcars.cv5.rf$mpg)
 #' plot(mtcars.cv5.rf$rf.pred.mpg, mtcars.cv5.rf$mpg, main = "mpg predictions, 5-fold cross validation, random forest", xlab = "predicted", ylab = "actual")
 #'
-#' if (!require(FNN)) install.packages('FNN'
+#' if (!require(FNN)) install.packages('FNN')
 #' library(FNN)
 #' mtcars.loocv.knn4 <- crossValidate(df = mtcars, codeString = 'dtrain.x <- subset(traindata, select = -mpg); dtrain.y <- subset(traindata, select = mpg); dtest.x <- subset(testdata, select = -mpg); dtest.y <- subset(testdata, select = -mpg); pred1 <- FNN::knn.reg(dtrain.x, dtest.x, dtrain.y, k = 4); testdata$knn4.pred.mpg <- pred1$pred', numFolds = nrow(mtcars), randomSort = TRUE)
 #' if (!require(Metrics)) install.packages('Metrics')
 #' library(Metrics)
-#' Metrics::rmse(mtcars.loocv.knn4$knn4.pred.mpg, dtest.y$mpg)
-#' plot(mtcars.loocv.knn4$knn4.pred.mpg, dtest.y$mpg, main = "mpg predictions, LOOCV, KNN (k = 4)", xlab = "predicted", ylab = "actual")
+#' Metrics::rmse(mtcars.loocv.knn4$knn4.pred.mpg, mtcars$mpg)
+#' plot(mtcars.loocv.knn4$knn4.pred.mpg, mtcars$mpg, main = "mpg predictions, LOOCV, KNN (k = 4)", xlab = "predicted", ylab = "actual")
 #'
 crossValidate <- function(df, codeString, numFolds, randomSort = TRUE){
+
+
+  # Note for future versions:
+  # should also add into returned data.frame:
+  # new ID number (according to the reordered data), original order number,
+  # and fold (group) number.
 
   # Record original order of observations
   df$origOrder <- seq(1:nrow(df))
